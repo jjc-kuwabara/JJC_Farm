715500fb464e1a1655704f702a0895a00445a91f
JJC-kuwabara <kuwabara.ryosuke@nsg.gr.jp>
2023-06-09 01:57:27 +0900

    Initial commit

---
+++ Assets/Scripts/TomatoPlant.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 植物としてのトマトオブジェクトの派生クラス.
+public class TomatoPlant : BasePlant
+{
+    [SerializeField]
+    GameObject vegetable;
+    [SerializeField]
+    int maxVegetableNum;
+
+    override protected void CreateVegetable()
+    {
+        int vegetableNum = Random.Range(1, maxVegetableNum + 1);
+
+        for (int i = 0; i < vegetableNum; i++)
+        {
+            GameObject vegetableInstance = Instantiate(vegetable);
+            vegetableInstance.transform.SetParent(this.transform);
+            vegetableInstance.transform.localPosition = new Vector3(-0.3f + Random.Range(0.0f, 0.6f), -0.3f + Random.Range(0.0f, 0.6f), 0.0f);
+        }
+
+    }
+}
